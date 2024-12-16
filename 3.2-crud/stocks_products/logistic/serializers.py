bca2b7ee61cd206b23f6f4573184a71d7a5fea18
anazimov <nasimov.alexander@gmail.com>
2024-12-17 01:44:31 +0300

    hw6

---
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'products', 'positions']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for pos in positions:
+            StockProduct.objects.create(stock=stock, **pos)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for pos in positions:
+            StockProduct.objects.update(stock=stock, **pos, default={'price': .0, 'quantity': 0})
+
+        return stock
